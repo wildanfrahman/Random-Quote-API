ea4cdd38e1ad8a0c88e4834c976709ee3447faa0
Muhammad Wildan Fathur Rahman <muhammad.wildan.fr81@gmail.com>
2025-01-22 10:20:22 +0700

    add var prod

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,9 @@
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_DIALECT } = process.env;
+let config;
 
-module.exports = {
-  HOST: DB_HOST,
-  USER: DB_USER,
-  PASSWORD: DB_PASSWORD,
-  DB: DB_NAME,
-  dialect: DB_DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+if (process.env.NODE_ENV === "production") {
+  config = require("./db.config.prod");
+} else {
+  config = require("./db.config.dev");
+}
+
+module.exports = config;
--- app/models/index.js
+++ app/models/index.js
@@ -1,16 +1,31 @@
 const config = require("../config/db.config");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
-  dialect: config.dialect,
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
-});
+
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(config.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+    pool: {
+      max: config.pool.max,
+      min: config.pool.min,
+      acquire: config.pool.acquire,
+      idle: config.pool.idle,
+    },
+  });
+}
 
 const db = {};
 
